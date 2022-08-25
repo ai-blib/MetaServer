d60db1d47e10f6018d6e8a09e8b7286e02a0b7d3
hanyuhui <1608407759@qq.com>
2022-08-25 21:53:03 +0800

    fix server

--- server.js
+++ server.js
@@ -1,75 +1,60 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const socket = require('socket.io');
+var http = require('http');
+var express = require('express');
+var {RtcTokenBuilder, RtmTokenBuilder, RtcRole, RtmRole} = require('agora-access-token')
 
-const port = 3007;
-app.all('*', function (req, res, next) {
-    if (!req.get('Origin')) return next();
-    // use "*" here to accept any origin
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET');
-    res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-    // res.set('Access-Control-Allow-Max-Age', 3600);
-    if ('OPTIONS' == req.method) return res.send(200);
-    next();
+var PORT = 8089;
+
+// Fill the appID and appCertificate key given by Agora.io
+var appID = "7a9157a0e65a49e9aab58b62f1f608f4";
+var appCertificate = "f328737a289e4cceb11cda29d5bf0dc0";
+
+// token expire time, hardcode to 3600 seconds = 1 hour
+var expirationTimeInSeconds = 3600
+var role = RtcRole.PUBLISHER
+
+var app = express();
+app.disable('x-powered-by');
+app.set('port', PORT);
+app.use(express.favicon());
+app.use(app.router);
+
+var generateRtcToken = function(req, resp) {
+    var currentTimestamp = Math.floor(Date.now() / 1000)
+    var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
+    var channelName = req.query.channelName;
+    // use 0 if uid is not specified
+    var uid = req.query.uid || 0
+    if (!channelName) {
+        return resp.status(400).json({ 'error': 'channel name is required' }).send();
+    }
+
+
+    var key = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs);
+
+    resp.header("Access-Control-Allow-Origin", "*")
+    //resp.header("Access-Control-Allow-Origin", "http://ip:port")
+    return resp.json({ 'key': key }).send();
+};
+
+var generateRtmToken = function(req, resp) {
+    var currentTimestamp = Math.floor(Date.now() / 1000)
+    var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
+    var account = req.query.account;
+    if (!account) {
+        return resp.status(400).json({ 'error': 'account is required' }).send();
+    }
+
+    var key = RtmTokenBuilder.buildToken(appID, appCertificate, account, RtmRole, privilegeExpiredTs);
+
+    resp.header("Access-Control-Allow-Origin", "*")
+    //resp.header("Access-Control-Allow-Origin", "http://ip:port")
+    return resp.json({ 'key': key }).send();
+};
+
+app.get('/rtcToken', generateRtcToken);
+app.get('/rtmToken', generateRtmToken);
+
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('AgoraSignServer starts at ' + app.get('port'));
 });
-
-app.get('/hs', (req, res) => {
-    res.send('<p style="color:red">服务已启动</p>');
-})
-const httpServer = app.listen(3000, () => {
-    console.log('listen:30001');
-})
-const options = {
-    ioOptions: {
-        pingTimeout: 10000,
-        pingInterval: 5000,
-    },
-    origins:["127.0.0.1:8000",'localhost:3003'],
-    serveClient: false,
-    // below are engine.IO options
-    pingInterval: 20000,
-    pingTimeout: 5000,
-    cookie: false
-};
-const httpIo = socket(httpServer, {cors:true});
-const rooms = {};
-const socks = {};
-const httpConnectIoCallBack = (sock) => {
-    console.log(`sockId:${sock.id}连接成功!!!`);
-    sock.emit('connectionSuccess', sock.id);
-    // 用户断开连接
-    sock.on('userLeave', ({userName, galleryId, sockId} = user) => {
-        console.log(`userName:${userName}, galleryId:${galleryId}, sockId:${sockId} 断开了连接...`);
-        if (galleryId && rooms[galleryId] && rooms[galleryId].length) {
-            rooms[galleryId] = rooms[galleryId].filter(item => item.sockId !== sockId);
-            httpIo.in(galleryId).emit('userLeave', rooms[galleryId]);
-            console.log(`userName:${userName}, galleryId:${galleryId}, sockId:${sockId} 离开了房间...`);
-        }
-    });
-    // 用户加入房间
-    sock.on('join', ({userName, galleryId, sockId, point}) => {
-        rooms[galleryId] = rooms[galleryId] || [];
-        if (rooms[galleryId].some((user) => user.userName === userName)) {
-            return;
-        }
-        rooms[galleryId].push({userName, galleryId, sockId, point:[point,point]});
-        sock.join(galleryId);
-        httpIo.in(galleryId).emit('joinGallerySuccess', rooms[galleryId]);
-        socks[sockId] = sock;
-        console.log(`userName:${userName}, galleryId:${galleryId}, sockId:${sockId} 成功加入房间!!!`);
-    });
-    sock.on('sendPosition', ({galleryId, point, userName}) => {
-        const currentUser = rooms[galleryId].find((user) => user.userName === userName);
-        if (!currentUser) return;
-        currentUser.point.shift()
-        currentUser.point.push(point)
-        httpIo.in(galleryId).emit('userPosition', rooms[galleryId]);
-    });
-
-};
-httpIo.on('connection', httpConnectIoCallBack);
-
-
-
